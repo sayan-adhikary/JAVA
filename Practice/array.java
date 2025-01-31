2e9475aca52c055ea27174be740bdd3c4490a2c1
sayan-adhikary <pap9679429830@gmail.com>
2025-01-31 12:22:46 +0530

    Updated

---
+++ Practice/array.java
@@ -0,0 +1,10 @@
+package Practice;
+
+public class array {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30};
+        int n = arr.length;
+        int sum = arr[n-1] + arr[n-2];
+        System.out.println(sum);
+    }
+}

a17aa0e68252e46b8ae9ee4b3e4f8500c676aeb2
sayan-adhikary <pap9679429830@gmail.com>
2025-02-16 10:34:49 +0530

    Updated

--- Apna College/threeIsEven.java
+++ Apna College/threeIsEven.java
@@ -1,13 +1,13 @@
 import java.util.*;
 public class threeIsEven {
-    public static void main(String[]args){
+    public static void main(String[] args){
             Scanner sc = new Scanner(System.in);
             int x = 3;
             if (x % 2 == 0) {
                 System.out.print("Bazinga");
             }
             
-                System.out.println();
+                System.out.println("No");
     }
     
 }
--- Practice/largest.java
+++ Practice/largest.java
@@ -1,10 +1,17 @@
 import java.util.*;
 public class largest {
-    static void largest(int[] arr){
+    static void largestNumber(int[] arr){
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        int largest = Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i] > largest ){
+                largest = arr[i];
+            }
+        }
+        System.out.println(largest);
     }
     public static void main(String[] args) {
-        
+        int arr[] = {1,3,2,1,5,7};
+        largestNumber(arr);
     }
 }

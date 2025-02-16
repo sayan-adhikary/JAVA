ec7c08d7c7c3d81f64bd15a426b3ccf0d21eb63f
sayan-adhikary <pap9679429830@gmail.com>
2025-02-16 10:23:55 +0530

    Update largest.java

--- Practice/largest.java
+++ Practice/largest.java
@@ -1,6 +1,8 @@
+import java.util.*;
 public class largest {
     static void largest(int[] arr){
-        
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
     }
     public static void main(String[] args) {
         

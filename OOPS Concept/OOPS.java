9332b0879254d09588cc38d53059a6c5230ca9d4
sayan-adhikary <pap9679429830@gmail.com>
2025-02-27 13:44:21 +0530

    OOPS

--- OOPS Concept/OOPS.java
+++ OOPS Concept/OOPS.java
@@ -4,14 +4,25 @@ class Pen{
     public void write(){
         System.out.println("Write sometging");
     }
+
+    public void printColor(){
+        System.out.println(this.color);
+    }
 }
 
-public class main {
+public class OOPS {
     public static void main(String args[]) {
         Pen pen1 = new Pen();
         pen1.color = "red";
         pen1.type = "gel";
         pen1.write();
+
+        Pen pen2 = new Pen();
+        pen2.color = "black";
+        pen2.type = "blue";
+
+        pen1.printColor();
+        pen2.printColor();
     }
 }
 

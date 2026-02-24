d39018e884ed28a770df43b9678c8fc5774fac90
sayan-adhikary <103528635+sayan-adhikary@users.noreply.github.com>
2026-02-24 22:56:46 +0530

    Create palindromeNumber.java

---
+++ TUF/palindromeNumber.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class palindromeNumber {
+    public static boolean isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+
+        int reversed = 0;
+        while (x > reversed) {
+            int digit = x % 10;
+            reversed = reversed * 10 + digit;
+            x /= 10;
+        }
+
+        return x == reversed || x == reversed / 10;
+    }
+
+}
